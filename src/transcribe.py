4537a9da83b2fea7e810d19cb9d9e2605855d03a
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-09-20 15:36:12 +0300

    Update transcribe.py to streamline model download and remove obsolete commands for whisper-heb-ipa; adjust script for improved functionality.

--- src/transcribe.py
+++ src/transcribe.py
@@ -8,13 +8,9 @@ OR
 
 git clone https://huggingface.co/thewh1teagle/phonikud-experiments
 
+uv run hf download --repo-type model thewh1teagle/whisper-heb-ipa-ct2 --local-dir ./whisper-heb-ipa-ct2
 uv run src/transcribe.py --input ./phonikud-experiments/comparison/audio/ --output ./transcripts
 
-git clone https://huggingface.co/thewh1teagle/whisper-heb-ipa
-uv run ct2-transformers-converter \
-    --model ./whisper-heb-ipa \
-    --output_dir ./whisper-heb-ipa-ct2 \
-    --quantization int8_float16
 """
 import argparse
 import torch
